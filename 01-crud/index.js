63a5c8544a9527dc5af5de2f0a70474db1c3ba36
vibhuti-jadav <vibhutij109@gmail.com>
2025-08-29 18:00:20 +0530

    crud

--- 01-crud/index.js
+++ 01-crud/index.js
@@ -8,6 +8,38 @@ app.get("/",(req,res)=>{
     res.send("hello from server")
 });
 
+const students =[
+    {
+        id:1,
+        name:"vibhuti",
+    },
+    {
+        id:2,
+        name:"nihali",
+    },
+];
+
+app.get("/student",(req,res)=>{
+    if(students.length === 0){
+        res.send("no student found");
+    }
+    res.json(students);
+});
+
+app.get("/student/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+    console.log("id",id);
+
+    const students = students.find((std)=>std.id === id);
+
+    if(!students){
+        res.json("id not found");
+        return;
+    }
+
+    res.json({message:"student data found",students});
+
+})
 
 const port= process.env.PORT || 5000;
 
